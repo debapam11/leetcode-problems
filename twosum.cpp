f677e0935ef8d9d12250ee33b6a0175b87a35c94
debapam11 <debapam11@gmail.com>
2020-10-05 04:06:19 +0530

    added twosum solution

--- twosum.cpp
+++ twosum.cpp
@@ -1,19 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(void){
+vector<int> twosum(vector<int>& num, int target){
+    vector<int> result;          //vector to store the result indices
+    vector<int> failed {-1,-1};    //just a vector to return in case sum cannot be attained
+
+    unordered_map<int,int> hold;    //unordered_map to hold the value, index pair
+    int i=0;
+    while(i<num.size()){
+        int diff = target - num[i];
+        if(hold.find(diff)!=hold.end()){        //if target diff is found in hashmap, append the 
+            result.push_back(hold[diff]);       //index to the result vector and return
+            result.push_back(i);
+            return result;
+        }
+        else
+        {                                       //if target difference not found, add the current value to  
+            hold[num[i]]=i;                     // the hash map and keep going :)
+            ++i;
+        }
+        
+    }
+    return failed;
+}
+
+
+int main(){
     vector<int> v{9, 6, 4, 2};
     vector<int> res;
-    int target,f,i=1;
+    int target;
+    cout<<"Enter target value: ";
     cin>>target;
-    unordered_set<int> database;
-    //unordered_set<string> :: iterator itr;
-    database.insert(v[0]);
-    while(i<v.size()){
-        if(database.find(target-v[i])!=database.end()){
-            auto itr=database.find(target-v[i]);
-            cout<<(*itr)<<" "<<v[i];
-        }
-        ++i;
+    res=twosum(v,target);
+    for(int i=0;i<2;i++){
+        cout<<res[i]<<"\t";
     }
+    return 0;
 }
